776622e3fbf9eeb88185d45b6f8d6bb90e421954
SabaShahzadi90 <sshahzadi714@gmail.com>
2026-08-25 10:05:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config(); // dotenv use karo
-
-const url = process.env.MONGO_URL; // .env se lo
-const dbName = 'giftdb'; // database ka naam giftdb ho
-
-let client;
-
 async function connectToDatabase() {
     client = new MongoClient(url);
     await client.connect(); // ye line zaroori hai
     console.log('Connected to MongoDB');
     return client.db(dbName);
 }
+
+module.exports = connectToDatabase; // YE LINE ADD KARO
